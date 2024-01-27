7fb26f74a852f49c8cd24fbe13c2cebb15cb777b
Landon Hughes <landonhughesdeveloper@gmail.com>
2024-01-27 13:19:35 -0500

    add boilerplate

---
+++ django_boilerplate/settings.py
@@ -0,0 +1,135 @@
+
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+# Env variables
+ENGINE = os.getenv("ENGINE")
+DATABASE_NAME = os.getenv("NAME")
+PASSWORD = os.getenv("PASSWORD")
+HOST = os.getenv("HOST")
+PORT = os.getenv("PORT")
+SECRET = os.getenv("SECRET_KEY")
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = SECRET
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'django_boilerplate.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates']
+        ,
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'django_boilerplate.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': ENGINE,
+        'NAME': DATABASE_NAME,
+        'PASSWORD': PASSWORD,
+        'HOST': HOST,
+        'PORT': PORT
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.0/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+# You can think of this like the "where", as in where should our links look if we add a logo to the home page.
+STATIC_URL = 'static/'
+# You can also think of this like the "where, as in what url should user uploaded images be pointed to. So, say a user uploaded an image called egg.png, the location of that image would look something like www.myfancywebsite.com/images/egg.png.
+MEDIA_URL = "/images/"
+# Inside our Django project, we need JS, CSS, and static content to make our site look fancy. You can think of the line below to tell Django "Hey, look here for these files".
+STATICFILES_DIRS = [BASE_DIR / "static"]
+# The line below tells Django, "Hey, store any user uploaded content to <root-folder-project-name>/static/images"
+MEDIA_ROOT = BASE_DIR / "static/images"
+# STATIC_ROOT is the exact same thing except, this is where our static files will go when we use `python manage.py collectstatic`.
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
